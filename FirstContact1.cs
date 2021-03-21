e59d0de0ddf1f0073627be987da15b2d72b639ea
fumi1623 <ancient17.hirofumi@gmail.com>
2021-03-21 23:44:15 +0900

    [restart]

---
+++ FirstContact1.cs
@@ -0,0 +1,141 @@
+﻿//using System;
+
+//namespace FirstContact_Csharp
+//{
+//    class Program
+//    {
+//        static void Main()
+//        {
+//            /*--1--
+//            //string msg;
+//            //msg = "Hello World!";
+//            //string msg = "Hello World!";
+//            const string msg = "Hello World!";
+//            Console.WriteLine(msg);
+//            */
+
+//            /*--2--
+//            string s = "hello";
+//            char c = 'a';
+//            int i = 100;
+//            double d = 52342.34;
+//            float f = 23.3f;
+//            bool flag = true;
+//            var x = 5;
+//            var y = "world";
+//            */
+
+//            /*--3--
+//            var x = 10;
+//            Console.WriteLine(x / 3);
+//            Console.WriteLine(x % 3);
+//            Console.WriteLine(x / 3.0);
+//            Console.WriteLine(x / (double)3);
+
+//            var y = 5;
+//            y++;
+//            Console.WriteLine(y);
+
+//            var z = 6;
+//            //z = z + 10;
+//            z += 10;
+//            Console.WriteLine(z);
+//            */
+
+//            /*--4--
+//            //Console.WriteLine("hello " + "world");
+//            //Console.WriteLine("hello\nworld");
+
+//            var name = "taguchi";
+//            var score = 52.3;
+
+//            //Console.WriteLine(string.Format("{0} [{1}]", name, score));
+//            Console.WriteLine($"{name} [{score}]");
+//            Console.WriteLine($"{name, 10} [{score, -10}]");
+//            */
+
+//            /*--5--
+//            //var score = int.Parse(Console.ReadLine());
+
+//            //if (score > 80) {
+//            //    Console.WriteLine("Greate!");
+//            //} else if (score > 60) {
+//            //    Console.WriteLine("good");
+//            //} else {
+//            //    Console.WriteLine("so so...");
+//            //}
+
+//            //Console.WriteLine((score > 80) ? "Great!": "so so ...");
+//            */
+
+//            /*--6--
+//            var signal = Console.ReadLine();
+
+//            switch(signal) {
+//                case "red":
+//                    Console.WriteLine("Stop!");
+//                    break;
+//                case "yellow":
+//                    Console.WriteLine("Caution");
+//                    break;
+//                case "blue":
+//                case "green":
+//                    Console.WriteLine("Go!");
+//                    break;
+//                default:
+//                    Console.WriteLine("wrong signal!");
+//                    break;
+//            }
+//            */
+
+//            /*--7--
+//            //var i = 0;
+//            //while (i < 10) {
+//            //    Console.WriteLine(i);
+//            //    i++;
+//            //}
+
+//            //do {
+//            //    Console.WriteLine(i);
+//            //    i++;
+//            //} while (i < 10);
+//            */
+
+//            /*--8--
+//            for (int i = 0; i < 10; i++) {
+//                if (i == 5) {
+//                    //continue;
+//                    break;
+//                }
+//                Console.WriteLine(i);
+//            }
+//            */
+
+//            /*--9--
+//            //int[] scores = new int[3];
+//            //scores[0] = 10;
+//            //scores[1] = 30;
+//            //scores[2] = 20;
+//            //int[] scores = new int[] {10, 30, 20};
+//            //int[] scores = {10, 30, 20};
+//            //var scores = new[] { 10, 30, 20 };
+
+//            //scores[1] = 40;
+//            //Console.WriteLine(scores[1]);
+//            */
+
+//            /*--10--
+//            var scores = new[] { 10, 30, 20 };
+
+//            for (int i = 0; i < scores.Length; i++) {
+//                Console.WriteLine(scores[i]);
+//            }
+
+//            foreach (int score in scores) {
+//                Console.WriteLine(score);
+//            }
+//            */
+
+//        }
+//    }
+//}
---
+++ FirstContact2.cs
@@ -0,0 +1,214 @@
+﻿//using System;
+
+//namespace FirstContact_Csharp2 {
+//    class Program {
+
+
+//        //static void SayHi() {
+//        //    Console.WriteLine("hi!");
+//        //}
+//        //static string SayHi() {
+//        //    return "hi!";
+//        //}
+//        static string SayHi() => "hi!";
+
+
+//        static void Main()
+//        {
+//            Console.WriteLine(SayHi());
+
+
+//        }
+//    }
+//}
+
+//---2
+//using System;
+
+//namespace FirstContact_Csharp2 {
+//    class Program {
+
+
+//        static void SayHi(string name, int age = 23) {
+//            Console.WriteLine($"hi! {name} ({age})");
+//        }
+
+//        static void Main() {
+//            SayHi("Tom", 30);
+//            SayHi("Tom");
+//            SayHi(age: 26, name: "steve");
+
+//        }
+//    }
+//}
+
+//---2
+//using System;
+
+//namespace FirstContact_Csharp2 {
+
+//    class User {
+//        public string name = "me";
+//        public void SayHi() {
+//            //Console.WriteLine($"hi {this.name}");
+//            Console.WriteLine($"hi {name}");
+//        }
+//    }
+
+//    class Program {
+
+//        static void Main() {
+//            User user = new User();
+//            Console.WriteLine(user.name);
+//            user.SayHi();
+//        }
+//    }
+//}
+
+//-----
+//using System;
+
+//namespace FirstContact_Csharp2 {
+
+//    class User {
+//        public string name;
+//        //public User() {
+//        //    this.name = "ME";
+//        //}
+//        public User(string name) {
+//            this.name = name;
+//        }
+//        //public User() {
+//        //    this.name = "nobody";
+//        //}
+//        public User(): this("nobody") {
+
+//        }
+//        public void SayHi() {
+//            Console.WriteLine($"hi {name}");
+//        }
+//    }
+
+//    class Program {
+
+//        static void Main() {
+//            //User user = new User();
+//            //user.SayHi();
+//            User tom = new User("Tom");
+//            tom.SayHi();
+//            User user = new User();
+//            user.SayHi();
+//        }
+//    }
+//}
+
+
+//-----
+//using System;
+
+//namespace FirstContact_Csharp2 {
+
+//    class User {
+//        public string name;
+//        public User(string name) {
+//            this.name = name;
+//        }
+//        public virtual void SayHi() {
+//            Console.WriteLine($"hi {name}");
+//        }
+//    }
+
+//    class AdminUser: User {
+//        public AdminUser(string name): base(name) {
+//        }
+//        public void SayHello() {
+//            Console.WriteLine($"hello {name}");
+//        }
+//        public override void SayHi() {
+//            Console.WriteLine($"[admin] hi {name}");
+//        }
+//    }
+
+//    class Program {
+
+//        static void Main() {
+//            User tom = new User("Tom");
+//            tom.SayHi();
+//            AdminUser bob = new AdminUser("Bob");
+//            bob.SayHi();
+//            bob.SayHello();
+//        }
+//    }
+//}
+
+//------
+//using System;
+
+//namespace FirstContact_Csharp2 {
+
+//    class User {
+//        //public string name;
+//        //protected string name;
+//        private string name;
+//        public User(string name) {
+//            this.name = name;
+//        }
+//        public virtual void SayHi() {
+//            Console.WriteLine($"hi {name}");
+//        }
+//    }
+
+//    class AdminUser : User {
+//        public AdminUser(string name) : base(name) {
+//        }
+//        public void SayHello() {
+//            Console.WriteLine($"hello {name}");
+//        }
+//        public override void SayHi() {
+//            Console.WriteLine($"[admin] hi {name}");
+//        }
+//    }
+
+//    class Program {
+
+//        static void Main() {
+//            User tom = new User("Tom");
+//            Console.WriteLine(tom.name);
+//            tom.SayHi();
+//            AdminUser bob = new AdminUser("Bob");
+//            bob.SayHi();
+//            bob.SayHello();
+//        }
+//    }
+//}
+
+using System;
+
+namespace FirstContact_Csharp2 {
+
+    class User {
+        private string name = "me!";
+
+        public string Name {
+            get { return this.name; }
+            set {
+                if (value != "") {
+                    this.name = value;
+                }
+            }
+        }
+        //public string Name { get; set; } = "me!";
+    }
+
+    class Program {
+
+        static void Main() {
+            User user = new User();
+            Console.WriteLine(user.Name);
+            user.Name = "taguchi";
+            Console.WriteLine(user.Name);
+            user.Name = "";
+            Console.WriteLine(user.Name);
+        }
+    }
+}
